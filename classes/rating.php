982101d084896ad6ddd996aaf0d51a0c204d1d27
folly77folly <folly77folly@gmail.com>
2020-11-28 14:24:56 +0100

    fixed-product

--- classes/rating.php
+++ classes/rating.php
@@ -1,5 +1,4 @@
 <?php
-// namespace App\Rating;
 class Rating{
 
     public function __construct($conn){
@@ -17,11 +16,11 @@ class Rating{
     public function ratingCount(int $id){
         $productCode = $this->productId($id);
         $sqlQuery = "SELECT count(*) from product_review where rvue_rating > 0 AND rvue_productid= '". $productCode . "'";
-        $result = \mysqli_query($this->conn, $sqlQuery);
+        $result = mysqli_query($this->conn, $sqlQuery);
         if (isset($result)){
 
-            if (\mysqli_num_rows($result) > 0){
-                $rows = \mysqli_fetch_row($result);
+            if (mysqli_num_rows($result) > 0){
+                $rows = mysqli_fetch_row($result);
                 return $rows[0];
             }
             return 0;
@@ -32,7 +31,7 @@ class Rating{
     public function productStar(int $id){
         $productCode = $this->productId($id);
         $sqlQuery = "SELECT AVG(rvue_rating) from product_review where rvue_productid = '". $productCode . "'";
-        $result = \mysqli_query($this->conn, $sqlQuery);
+        $result = mysqli_query($this->conn, $sqlQuery);
         if($result){
             $row = mysqli_fetch_row($result);
             $answer = number_format(floatval($row[0]), 1);
@@ -44,11 +43,11 @@ class Rating{
     public function reviewCount(int $id){
         $productCode = $this->productId($id);
         $sqlQuery = "SELECT count(*) from product_review where rvue_msg <> '' AND rvue_productid= '". $productCode . "'";
-        $result = \mysqli_query($this->conn, $sqlQuery);
+        $result = mysqli_query($this->conn, $sqlQuery);
         if (isset($result)){
 
-            if (\mysqli_num_rows($result) > 0){
-                $rows = \mysqli_fetch_row($result);
+            if (mysqli_num_rows($result) > 0){
+                $rows = mysqli_fetch_row($result);
                 return $rows[0];
             }
             return 0;
@@ -58,11 +57,11 @@ class Rating{
     public function allReviews(int $id){
         $productCode = $this->productId($id);
         $sqlQuery = "SELECT * from product_review where rvue_productid= '". $productCode . "'";
-        $result = \mysqli_query($this->conn, $sqlQuery);
+        $result = mysqli_query($this->conn, $sqlQuery);
         if (isset($result)){
 
-            if (\mysqli_num_rows($result) > 0){
-                $rows = \mysqli_fetch_array($result);
+            if (mysqli_num_rows($result) > 0){
+                $rows = mysqli_fetch_array($result);
                 return $rows;
             }
 
